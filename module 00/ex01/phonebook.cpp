fa1e4a9d036c8f1778e480b370bc7eb02c66b095
abdlalisalmi <abdlalisalmi@gmail.com>
2021-11-24 16:55:21 +0100

    working on phonebook

--- module 00/ex01/phonebook.cpp
+++ module 00/ex01/phonebook.cpp
@@ -6,23 +6,65 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 13:04:48 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/11/24 13:25:03 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:52:56 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
-#include <iostream>
+
+char *get_user_input()
+{
+    std::string input;
+    std::getline(std::cin, input);
+    return (char *)input.c_str();
+}
+
+void create_new_contact(PhoneBook phonbook)
+{
+    Contact contact;
+    std::cout << "First Name: ";
+    std::getline(std::cin, contact.first_name);
+
+    // std::cout << "Last Name: ";
+    // std::getline(std::cin, contact.last_name);
+
+    // std::cout << "Nickname: ";
+    // std::getline(std::cin, contact.nickname);
+    
+    // std::cout << "Phone Number: ";
+    // std::getline(std::cin, contact.phone_number);
+    
+    // std::cout << "Darkest: ";
+    // std::getline(std::cin, contact.darkest);
+    
+    // std::cout << "Secret: ";
+    // std::getline(std::cin, contact.secret);
+
+    phonbook.add_contact(contact);
+}
 
 int main()
 {
-    char *command;
+    PhoneBook phonebook;
+    Contact contact;
+    phonebook.index = 0;
+    phonebook.n_contacts = 0;
     while (TRUE)
     {
-        std::getline(std::cin, command);
-        if (strcmp(command, "EXIT"))
+        char *command;
+        
+        command = get_user_input();
+        if (strcmp(command, "EXIT") == 0)
             exit(EXIT_SUCCESS);
+        else if (strcmp(command, "ADD") == 0)
+            create_new_contact(phonebook);
+        else if (strcmp(command, "SEARCH") == 0)
+            std::cout << "SEARCH\n";
+        else
+            std::cout << "Invalid Command!!\n";
+
+        // for (int i = 0; i < phonebook.n_contacts; i++)
+        //     std::cout << phonebook.contacts[i].first_name <<'\n';
     }
-    
-    
     return 0;
 }
--- module 00/ex01/phonebook.hpp
+++ module 00/ex01/phonebook.hpp
@@ -1,6 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 16:05:44 by aes-salm          #+#    #+#             */
+/*   Updated: 2021/11/24 16:42:32 by aes-salm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-# define TRUE 1
+#include <iostream>
+
+#define TRUE 1
+
+class Contact
+{
+public:
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest;
+    std::string secret;
+};
+
+class PhoneBook
+{
+public:
+    int index;
+    int n_contacts;
+    Contact contacts[8];
+
+    void add_contact(Contact contact)
+    {
+        contacts[index] = contact;
+        if (index == 8)
+            index = 0;
+        if (n_contacts < 8)
+            n_contacts++;
+    }
+};
+
+char *get_user_input();
 
 #endif
