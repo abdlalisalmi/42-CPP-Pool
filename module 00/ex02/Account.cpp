f041ed497cdc8a76feb9c94b095e617e21d38c1e
abdlalisalmi <abdlalisalmi@gmail.com>
2021-11-25 23:17:55 +0100

    finish the Account.cpp

--- module 00/ex02/Account.cpp
+++ module 00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 13:31:33 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/11/25 15:58:05 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/11/25 23:16:44 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,14 @@ Account::Account(int initial_deposit) {
     _nbWithdrawals = 0;
     
     _displayTimestamp();
-    std::cout << "Index:" << _accountIndex << ";amount:" << _amount << ";created\n";
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
 }
 
 Account::~Account(void) {
     _displayTimestamp();
-    std::cout << "Index:" << _accountIndex << ";amount:" << _amount << ";closed\n";
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
 void Account::_displayTimestamp(void) {
@@ -37,13 +39,33 @@ void Account::_displayTimestamp(void) {
     
     tm *time = localtime(&now);
 
+    std::string month = std::to_string(time->tm_mon + 1);
+    if (month.length() == 1)
+        month = '0' + month;
+
+    std::string day = std::to_string(time->tm_mday);
+    if (day.length() == 1)
+        day = '0' + day;
+
+    std::string hour = std::to_string(time->tm_hour);
+    if (hour.length() == 1)
+        hour = '0' + hour;
+
+    std::string minutes = std::to_string(time->tm_min);
+    if (minutes.length() == 1)
+        minutes = '0' + minutes;
+
+    std::string seconds = std::to_string(time->tm_sec);
+    if (seconds.length() == 1)
+        seconds = '0' + seconds;
+
     std::cout   <<"[" << 1900 + time->tm_year
-                << 1 + time->tm_mon
-                << time->tm_mday
+                << month
+                << day
                 << '_'
-                << time->tm_hour
-                << time->tm_min
-                << time->tm_sec
+                << hour
+                << minutes
+                << seconds
                 << "] ";
 }
 
@@ -61,8 +83,52 @@ int Account::getNbWithdrawals(void) {
 }
 void Account::displayAccountsInfos(void) {
     _displayTimestamp();
-    std::cout   << "accounts:" << _nbAccounts + 1
+    std::cout   << "accounts:" << _nbAccounts
                 << ";total:" << _totalAmount
                 << ";deposits:" << _totalNbDeposits
                 << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+void Account::makeDeposit(int deposit) {
+    _displayTimestamp();
+    std::cout   << "index:" << _accountIndex
+                << ";p_amount:" << _amount
+                << ";deposit:" << deposit;
+    _amount += deposit;
+    _nbDeposits++;
+    _totalAmount += deposit;
+    _totalNbDeposits++;
+    std::cout   << ";amount:" << _amount
+                << ";nb_deposits:" << _nbDeposits
+                << std::endl;
+}
+bool Account::makeWithdrawal(int withdrawal) {
+    _displayTimestamp();
+    std::cout   << "index:" << _accountIndex
+                << ";p_amount:" << _amount
+                << ";withdrawal:";
+    if (_amount >= withdrawal) {
+        _amount -= withdrawal;
+        _nbWithdrawals++;
+        _totalNbWithdrawals++;
+        _totalAmount -= withdrawal;
+        std::cout   << withdrawal
+                    <<";amount:" << _amount
+                    << ";nb_withdrawals:" << _nbWithdrawals
+                    << std::endl;
+        return true;
+    } else {
+        std::cout << "refused" << std::endl;
+        return false;
+    }
+}
+int Account::checkAmount(void) const {
+    return _amount;
+}
+void Account::displayStatus(void) const {
+    _displayTimestamp();
+    std::cout   << "index:" << _accountIndex
+                << ";amount:" << _amount
+                << ";deposits:" << _nbDeposits
+                << ";withdrawals:" << _nbWithdrawals
+                << std::endl;
 }
--- module 00/ex02/Account.hpp
+++ module 00/ex02/Account.hpp
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <string>
 
 // ************************************************************************** //
 //                               Account Class                                //
