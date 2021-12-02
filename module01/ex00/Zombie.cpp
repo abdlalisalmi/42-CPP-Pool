94f8d3132df1c3cb9d4df4f96c2593264d0872b7
abdlalisalmi <abdlalisalmi@gmail.com>
2021-12-02 12:06:03 +0100

    update ex00 Zombie class

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -6,20 +6,32 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 22:53:53 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/01 13:44:28 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/02 12:04:19 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name) {
+Zombie::Zombie(void)
+{
+}
+
+Zombie::Zombie(std::string name)
+{
     _name = name;
 }
 
-Zombie::~Zombie() {
+Zombie::~Zombie()
+{
     std::cout << "Destructor called on zombie " << _name << std::endl;
 }
 
-void Zombie::announce(void) {
+void Zombie::setName(std::string const name)
+{
+    this->_name = name;
+}
+
+void Zombie::announce(void)
+{
     std::cout << _name << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 22:46:18 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/01 13:44:36 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/02 12:04:47 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,12 @@ class Zombie
 private:
     std::string _name;
 public:
-    Zombie( std::string name );
+    Zombie( void );
+    Zombie(std::string name);
     ~Zombie();
-    void announce( void );
+    
+    void setName(std::string const name);
+    void announce(void);
 };
 
 #endif
