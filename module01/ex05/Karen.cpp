8d6d50811665e4a96c157ba20138b243469eb3f8
abdlalisalmi <abdlalisalmi@gmail.com>
2021-12-03 11:37:54 +0100

    finish ex05 in module01

--- module01/ex05/Karen.cpp
+++ module01/ex05/Karen.cpp
@@ -6,35 +6,48 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 00:05:28 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/03 00:32:23 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/03 11:37:18 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
+typedef void (Karen::*ptrToMuFunc)(void);
+
 Karen::Karen(void){
 }
-
 Karen::~Karen(){
 }
 
 void Karen::debug(void) {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 void Karen::info(void) {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
 }
 
 void Karen::warning(void) {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error(void) {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Karen::complain(std::string level) {
-    // const enum log {debug=debug, info=info};
-    std::cout << level << std::endl;
+    ptrToMuFunc methods[4] = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (size_t i = 0; i < 4; i++)
+    {
+        while (level == levels[i])
+        {
+            (this->*methods[i])();
+            break;
+        }
+    }
 }
