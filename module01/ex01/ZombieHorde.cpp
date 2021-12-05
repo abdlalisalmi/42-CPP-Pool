9f3a3d6d26cbef7b738a4d3b8d447e5b1536e6bd
abdlalisalmi <abdlalisalmi@gmail.com>
2021-12-05 19:43:10 +0100

    working on module02 ex00

--- module01/ex01/ZombieHorde.cpp
+++ module01/ex01/ZombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 13:24:55 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/02 12:08:24 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/04 13:37:00 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Zombie *zombieHorde(int N, std::string name)
     Zombie *zombie_horde = new Zombie [N];
 
     for (int i = 0; i < N; i++)
-        zombie_horde[i].setName(name + std::to_string(i));
-        
+        zombie_horde[i].setName(name);
+
     return zombie_horde;
 }
