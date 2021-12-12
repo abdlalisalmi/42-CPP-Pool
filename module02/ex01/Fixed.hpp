5fd66d58b0b6fe01efb8f0b322be68eb5eca60b8
abdlalisalmi <abdlalisalmi@gmail.com>
2021-12-12 15:28:38 +0100

    update ex00 and working on ex01 in module02

--- module02/ex01/Fixed.hpp
+++ module02/ex01/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/12 12:28:48 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/12 15:00:29 by aes-salm         ###   ########.fr       */
+/*   Created: 2021/12/12 14:38:45 by aes-salm          #+#    #+#             */
+/*   Updated: 2021/12/12 14:42:22 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ class Fixed {
     private:
         int _fp;
         static const int _nf = 8;
-    
+
     public:
         Fixed( void );
         Fixed( const Fixed &obj );
         ~Fixed( void );
-        
+
         Fixed &operator=( Fixed &obj);
         int getRawBits( void ) const;
         void setRawBits( int const raw );
