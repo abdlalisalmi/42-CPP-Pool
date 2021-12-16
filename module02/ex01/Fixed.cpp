9e36347c3d3ff192ccb73b57678e4489081eb9ef
abdlalisalmi <abdlalisalmi@gmail.com>
2021-12-16 20:42:19 +0100

    finish the ex01 in module02

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -6,25 +6,25 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 14:43:15 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/14 01:38:15 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/16 20:39:39 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-#include <iostream>
 
 const int Fixed::_fracBits = 8;
 
+// Default constructor
 Fixed::Fixed(void) {
     std::cout << "Default constructor called" << std::endl;
     this->_fixedPointValue = 0;
 }
-
+// Copy constructor
 Fixed::Fixed(const Fixed &obj) {
     std::cout << "Copy constructor called" << std::endl;
     *this = obj;
 }
-
+// Destructor
 Fixed::~Fixed() {
     std::cout << "Destructor called" << std::endl;
 }
@@ -37,12 +37,37 @@ Fixed &Fixed::operator=(const Fixed &obj) {
     return *this;
 }
 
+std::ostream &operator<<(std::ostream &output, const Fixed &obj) {
+    output << obj.toFloat();
+    return output;
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    this->_fixedPointValue = value << _fracBits;
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    this->_fixedPointValue = roundf(value * (1 << _fracBits));
+}
+
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
+    // std::cout << "getRawBits member function called" << std::endl;
     return this->_fixedPointValue;
 }
 
 void Fixed::setRawBits( int const raw) {
-    std::cout << "getRawBits member function called" << std::endl;
+    // std::cout << "getRawBits member function called" << std::endl;
     this->_fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return ((float)(this->_fixedPointValue) / (1 << _fracBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return ((int)(this->_fixedPointValue >> _fracBits));
 }
--- module02/ex01/Fixed.hpp
+++ module02/ex01/Fixed.hpp
@@ -6,13 +6,16 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 14:38:45 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/14 01:37:52 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/12/16 20:34:58 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef FIXED_HPP
 #define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
 
 class Fixed {
     private:
@@ -22,11 +25,18 @@ class Fixed {
     public:
         Fixed( void );
         Fixed( const Fixed &obj );
+        Fixed( const int value);
+        Fixed( const float value);
         ~Fixed( void );
         
         Fixed &operator=( const Fixed &obj);
+
+        float toFloat( void ) const;
+        int toInt( void ) const;
         int getRawBits( void ) const;
         void setRawBits( int const raw );
 };
+
+std::ostream &operator<<( std::ostream& cout, const Fixed &obj);
 
 #endif
