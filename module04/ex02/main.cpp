11add788e0879d4095da18c4df59268f0c9ccbd2
abdlalisalmi <abdlalisalmi@gmail.com>
2022-02-07 11:53:47 +0100

    fixing some issusses in ex01 in module4

--- module04/ex02/main.cpp
+++ module04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 16:27:59 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/01/22 20:14:59 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/02/07 11:53:12 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,51 @@
 
 int main()
 {
+	std::cout << std::endl;
+
+	// Animal test;
+
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
+	std::cout << "Create Dog and Cat Objects" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
 	const Animal *j = new Dog();
 	const Animal *i = new Cat();
 	delete j; // should not create a leak
 	delete i;
+	std::cout << std::endl;
 
-	std::cout << "############# Create array of animals[4] half Dogs and half Cats" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
+	std::cout << "Create array of animals[4] half Dogs and half Cats" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
 
 	Animal *animals[4];
 	for (size_t i = 0; i < 2; i++)
 		animals[i] = new Dog();
 	for (size_t i = 2; i < 4; i++)
 		animals[i] = new Cat();
+	std::cout << std::endl;
 
-	std::cout << "############# Delete the Dogs and the Cats array as an animal" << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
+	std::cout << "Delete the Dogs and the Cats array as an animal" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
 	for (size_t i = 0; i < 4; i++)
 		delete animals[i];
+	std::cout << std::endl;
 
-	std::cout << "############# Test the deep of Dog and Cat copy" << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
+	std::cout << "Test the deep of Dog and Cat copy" << std::endl;
+	std::cout << "----------------------------------------------" << std::endl;
 	Cat basic;
 	{
 		Cat tmp = basic;
 	}
-	std::cout << "######################" << std::endl;
+
+	std::cout << std::endl;
+	system("leaks a.out");
+	std::cout << std::endl;
 
 	return 0;
 }
