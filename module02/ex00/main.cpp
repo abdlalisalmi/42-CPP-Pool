0cbe806bb5be21b8657dd64d6901bf8dd7fd93b4
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-05 12:06:57 +0100

    update m02

--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 12:38:53 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/12/14 01:32:50 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/05 11:07:29 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main(void)
 {
     Fixed a;
     Fixed b(a);
-    Fixed c;
-    c = b;
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
+    // Fixed c;
+    // c = b;
+    // std::cout << a.getRawBits() << std::endl;
+    // std::cout << b.getRawBits() << std::endl;
+    // std::cout << c.getRawBits() << std::endl;
     return 0;
 }
