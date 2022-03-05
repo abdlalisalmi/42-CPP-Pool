74ae3aec56dbaace5450df995d65c4b0e392a36b
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-05 23:14:31 +0100

    finish the ex00 in the module07

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 22:42:05 by aes-salm          #+#    #+#             */
+/*   Updated: 2022/03/05 23:13:26 by aes-salm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename S>
+void swap(S &a, S &b) {
+	S tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename MIN>
+MIN min(MIN &a, MIN &b) {
+	return (a < b) ? a : b;
+}
+
+template <typename MAX>
+MAX max(MAX &a, MAX &b) {
+	return (a > b) ? a : b;
+}
+
+#endif
