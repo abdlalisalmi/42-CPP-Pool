da7eefd83c849d283fc247ec141f5fc5066937c4
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-08 12:00:13 +0100

    testing for final push

--- module07/ex00/whatever.hpp
+++ module07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 22:42:05 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/07 22:31:40 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/08 11:55:33 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 
 #include <iostream>
 
-template <typename S>
-void swap(S &a, S &b) {
-	S tmp;
+template <typename T>
+void swap(T &a, T &b) {
+	T tmp;
 	tmp = a;
 	a = b;
 	b = tmp;
 }
 
-template <typename MIN>
-MIN min(MIN &a, MIN &b) {
+template <typename T>
+T min(T &a, T &b) {
 	return (a < b) ? a : b;
 }
 
-template <typename MAX>
-MAX max(MAX &a, MAX &b) {
+template <typename T>
+T max(T &a, T &b) {
 	return (a > b) ? a : b;
 }
 
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 19:44:37 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/06 21:11:26 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/08 11:57:43 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int main() {
 	std::string words[] = { "hello", "world"};
 	int tab[] = { 0, 1, 2, 3, 4 };
 	Awesome tab2[5];
+
 	::iter( words, 2, print );
 	::iter( tab, 5, print );
 	::iter( tab2, 5, print );
