fa844a1cdcb8aad4cc79d645deaca2b704e47863
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-11 18:21:35 +0100

    update

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 18:18:22 by aes-salm          #+#    #+#             */
+/*   Updated: 2022/03/11 18:20:39 by aes-salm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+void easyFind(T iter, int )
+
+#endif
