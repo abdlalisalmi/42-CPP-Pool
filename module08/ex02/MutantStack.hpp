8c9ed321d500ce1d3bbd5ef404259ea477eeaed4
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-20 20:10:14 +0100

    working on the ex02

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 19:07:20 by aes-salm          #+#    #+#             */
+/*   Updated: 2022/03/20 19:14:41 by aes-salm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+
+class MutantStack
+{
+	private:
+		/* data */
+	public:
+		MutantStack(/* args */);
+		~MutantStack();
+};
+
+#endif
