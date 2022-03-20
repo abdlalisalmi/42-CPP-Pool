3aa4f1f49b87733a64581f855e689d669cf5923e
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-20 13:03:47 +0100

    finish the ex00

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 18:18:22 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/16 14:34:20 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/20 11:17:18 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ template <typename T>
 int easyFind(T vec, int occurrence) { // for handle Vectors and Arrays
 	typename T::iterator iter;
 	iter = std::find(vec.begin(), vec.end(), occurrence);
-	if (iter != vec.end()) return (iter - vec.begin());
-	throw std::runtime_error("Occurrence not found!");
+	if (iter == vec.end()) throw std::runtime_error("Occurrence not found!");
+	return (iter - vec.begin());
 }
 
 template <typename T>
@@ -48,6 +48,18 @@ int easyFind(std::queue<T> queue, int occurrence) { // for handle queues First-I
 	for (size_t i = 0; i < queue_len; i++)
 	{
 		vec.push_back(queue.front());
+		queue.pop();
+	}
+	return easyFind(vec, occurrence);
+}
+
+template <typename T>
+int easyFind(std::priority_queue<T> queue, int occurrence) { // for handle priority queues First-In First-Out
+	std::vector<T> vec;
+	size_t queue_len = queue.size();
+	for (size_t i = 0; i < queue_len; i++)
+	{
+		vec.push_back(queue.top());
 		queue.pop();
 	}
 	return easyFind(vec, occurrence);
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 09:31:30 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/16 14:32:02 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:29:50 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 int main() {
 	
+	std::cout << std::endl << "PS: Possition counting start from 0" << std::endl;
+
+	int occurrence = 5;
+	std::cout << std::endl << occurrence << " is the occurrence in all tests" << std::endl;
+
 	{
 		std::cout << std::endl << "------------ Test Vector ------------" << std::endl;
-		int occurrence = 5;
 		std::vector<int> vec;
 		for (int i = 1; i < 6; i++)
 			vec.push_back(i);
@@ -30,7 +34,6 @@ int main() {
 
 	{
 		std::cout << std::endl << "------------ Test Array ------------" << std::endl;
-		int occurrence = 3;
 		std::array<int, 5> arr = {1, 2, 3, 4, 5};
 		try {
 			int possiton = easyFind(arr, occurrence);
@@ -42,7 +45,6 @@ int main() {
 
 	{
 		std::cout << std::endl << "------------ Test Stack ------------" << std::endl;
-		int occurrence = 3;
 		std::stack<int> stack;
 		stack.push(1);
 		stack.push(2);
@@ -59,7 +61,6 @@ int main() {
 
 	{
 		std::cout << std::endl << "------------ Test Queue ------------" << std::endl;
-		int occurrence = 3;
 		std::queue<int> queue;
 		queue.push(1);
 		queue.push(2);
@@ -68,6 +69,22 @@ int main() {
 		queue.push(5);
 		try {
 			int possiton = easyFind(queue, occurrence);
+			std::cout << "Element " << occurrence << " found at position : " << possiton << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	{
+		std::cout << std::endl << "------------ Test Priority Queue ------------" << std::endl;
+		std::priority_queue<int> p_queue;
+		p_queue.push(1);
+		p_queue.push(2);
+		p_queue.push(3);
+		p_queue.push(4);
+		p_queue.push(5);
+		try {
+			int possiton = easyFind(p_queue, occurrence);
 			std::cout << "Element " << occurrence << " found at position : " << possiton << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << e.what() << std::endl;
