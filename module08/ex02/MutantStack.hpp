545ed710b0a3a2926223a826addecfe66da69865
abdlalisalmi <abdlalisalmi@gmail.com>
2022-03-21 21:12:37 +0100

    finish the ex02 in module08

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 19:07:20 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/20 19:14:41 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/21 21:08:08 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,36 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <stack>
+#include <list>
 
-class MutantStack
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 	private:
 		/* data */
 	public:
-		MutantStack(/* args */);
-		~MutantStack();
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		~MutantStack(){};
+		MutantStack(void){};
+		MutantStack(MutantStack const &other) {
+			*this = other;
+		};
+
+		MutantStack &operator=(MutantStack const &other) {
+			if (*this != other) {
+				this->c = other.c;
+			}
+			return *this;
+		};
+
+		iterator begin(void) {
+			return this->c.begin();
+		}
+		
+		iterator end(void) {
+			return this->c.end();
+		}
 };
 
 #endif
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,33 +6,60 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 19:05:50 by aes-salm          #+#    #+#             */
-/*   Updated: 2022/03/20 19:14:09 by aes-salm         ###   ########.fr       */
+/*   Updated: 2022/03/21 21:08:28 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
 int main() {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << std::endl << "------------- Test Stack -------------" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	{
+		std::cout << std::endl << "------------- Test List -------------" << std::endl;
+        std::list<int> mstack;
+        mstack.push_back(5);
+        mstack.push_back(17);
+        std::cout << mstack.back() << std::endl;
+        mstack.pop_back();
+        std::cout << mstack.size() << std::endl;
+        mstack.push_back(3);
+        mstack.push_back(5);
+        mstack.push_back(737);
+        //[...]
+        mstack.push_back(0);
+        std::list<int>::iterator it = mstack.begin();
+        std::list<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int, std::list<int> > s(mstack);
+    }
 	return 0;
 }
